a127260bc8d4d50d39cbf31b9b3fcdfa7f9f5025
hajarelamri <hajar.elamri@etu.uae.ac.ma>
2023-11-04 00:55:39 +0100

    hajar

--- src/main/java/com/group/Gestion/src/controller/AdminDashboardController.java
+++ src/main/java/com/group/Gestion/src/controller/AdminDashboardController.java
@@ -1,5 +1,7 @@
 package com.group.Gestion.src.controller;
 
+import com.group.Gestion.src.model.Employee;
+import com.group.Gestion.src.model.Status;
 import com.group.Gestion.src.service.EmployeeService;
 import com.group.Gestion.src.service.FeedbackService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -25,10 +28,31 @@ public class AdminDashboardController {
         if (selectedDate == null) {
             selectedDate = LocalDate.now().minusDays(1);  // Default to yesterday
         }
+        int workingEmployeeCount = 0;
+        int outsideEmployeeCount=0;
+        int inBreakEmployeeCount=0;
+        Status workingStatus = Status.EN_TRAVAIL;
+        Status inBreakStatus= Status.EN_PAUSE;
+        Status outsideStatus = Status.DEHORS;
 
         long totalEmployeeCount = employeeService.getTotalEmployeeCount();
         Map<String, Long> ratingStatistics = feedbackService.getRatingStatisticsForDate(selectedDate);
+        List<Employee> employees = employeeService.getEmployees();
 
+        for (Employee employee : employees) {
+            if (employee.getStatus() == workingStatus) {
+                workingEmployeeCount++;
+            }
+            if (employee.getStatus() == inBreakStatus) {
+                inBreakEmployeeCount++;
+            }
+            if(employee.getStatus() == outsideStatus){
+                outsideEmployeeCount++;
+            }
+        }
+        model.addAttribute("workingEmployeeCount", workingEmployeeCount);
+        model.addAttribute("outsideEmployeeCount", outsideEmployeeCount);
+        model.addAttribute("inBreakEmployeeCount", inBreakEmployeeCount);
         model.addAttribute("selectedDate", selectedDate);
         model.addAttribute("totalEmployeeCount", totalEmployeeCount);
         model.addAttribute("ratingStatistics", ratingStatistics);
